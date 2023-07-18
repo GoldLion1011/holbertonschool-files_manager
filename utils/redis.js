b6df04b1fcafbaa13aa1ec8fc49cd3c2c7d7e589
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-07-18 14:35:55 -0500

    aaaand im done for today

---
+++ utils/redis.js
@@ -0,0 +1,58 @@
+// Redis client
+
+const redis = require('redis');
+
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.error('Redis Error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+
+module.exports = redisClient;
