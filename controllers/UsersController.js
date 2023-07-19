13a5dc4f5586bb1ea9af2145723090f6046f6974
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-07-19 14:06:11 -0500

    task 3 redo

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,52 +1,103 @@
-// Defines our Users controller
-
-const { ObjectId } = require('mongodb');
 const sha1 = require('sha1');
+const mongo = require('mongodb');
+const Redis = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-const UsersController = {
-  async postNew(req, res) {
-    const { email, password } = req.body;
+class UsersController {
+  static postNew(req, res) {
+    (async () => {
+      const { email, password } = req.body;
 
-    if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
-    }
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
+      }
 
-    if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
-    }
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
 
-    const db = dbClient.client.db();
-    const usersCollection = db.collection('users');
+      const user = await dbClient.db.collection('users').findOne({ email });
+      if (user) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
 
-    // Check if email already exists in the database
-    const existingUser = await usersCollection.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ error: 'Already exist' });
-    }
+      const hash = sha1(password);
+      const newUser = await dbClient.db
+        .collection('users')
+        .insertOne({ email, password: hash });
+      return res.status(201).send({ id: newUser.insertedId, email });
+    })();
+  }
 
-    // Hash the password using SHA1
-    const hashedPassword = sha1(password);
+  static getMe(req, res) {
+    (async () => {
+      const token = req.headers['x-token'];
+      const user = await Redis.get(`auth_${token}`);
+      const userID = new mongo.ObjectID(user);
+      const userData = await dbClient.db
+        .collection('users')
+        .findOne({ _id: userID });
 
-    const newUser = {
-      email,
-      password: hashedPassword,
-    };
+      if (!userData) {
+        return res.status(401).send({ error: 'Unauthorized' });
+      }
 
-    // Save the new user to the database
-    try {
-      const result = await usersCollection.insertOne(newUser);
-      const { _id, email: insertedEmail } = result.ops[0];
-      const user = { id: _id, email: insertedEmail };
-
-      return res.status(201).json(user);
-    } catch (error) {
-      console.error('Error saving user to the database:', error);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-  },
-
-  // ... other endpoints ...
-};
+      return res.status(200).send({ id: userData._id, email: userData.email });
+    })();
+  }
+}
 
 module.exports = UsersController;
+
+// Defines our Users controller
+
+// const { ObjectId } = require('mongodb');
+// const sha1 = require('sha1');
+// const dbClient = require('../utils/db');
+
+// const UsersController = {
+//   async postNew(req, res) {
+//     const { email, password } = req.body;
+
+//     if (!email) {
+//       return res.status(400).json({ error: 'Missing email' });
+//     }
+
+//     if (!password) {
+//       return res.status(400).json({ error: 'Missing password' });
+//     }
+
+//     const db = dbClient.client.db();
+//     const usersCollection = db.collection('users');
+
+//     // Check if email already exists in the database
+//     const existingUser = await usersCollection.findOne({ email });
+//     if (existingUser) {
+//       return res.status(400).json({ error: 'Already exist' });
+//     }
+
+//     // Hash the password using SHA1
+//     const hashedPassword = sha1(password);
+
+//     const newUser = {
+//       email,
+//       password: hashedPassword,
+//     };
+
+//     // Save the new user to the database
+//     try {
+//       const result = await usersCollection.insertOne(newUser);
+//       const { _id, email: insertedEmail } = result.ops[0];
+//       const user = { id: _id, email: insertedEmail };
+
+//       return res.status(201).json(user);
+//     } catch (error) {
+//       console.error('Error saving user to the database:', error);
+//       return res.status(500).json({ error: 'Internal server error' });
+//     }
+//   },
+
+//   // ... other endpoints ...
+// };
+
+// module.exports = UsersController;
