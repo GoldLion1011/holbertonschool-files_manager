f1ef0d6cd1ec36a967299ed9ff8b9710e914d4a2
Alex Leeper <alixvox@gmail.com>
2023-07-24 03:40:27 -0500

    adding files for task 5

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
 
@@ -14,6 +15,7 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
 
 router.post('/users', UsersController.postNew);
 
