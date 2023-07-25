0247c843598c812780ce453b799f9ad6e53bccf3
Alex Leeper <alixvox@gmail.com>
2023-07-24 19:28:13 -0500

    Adding files & test files

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -108,7 +108,10 @@ const FilesController = {
   },
 
   async getIndex(req, res) {
-    const { parentId = '0', page = 0 } = req.query;
+    let { parentId = '0', page = 0 } = req.query;
+
+    // Convert page to number
+    page = Number(page);
 
     // Retrieve user based on the token
     const token = req.headers['x-token'];
@@ -125,7 +128,7 @@ const FilesController = {
     // Find files based on the parentId and userId with pagination
     let query = { userId };
     if (parentId !== '0') {
-      query.parentId = parentId;
+      query.parentId = ObjectId(parentId);
     }
 
     const files = await dbClient.db.collection('files')
